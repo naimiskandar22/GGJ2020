1a4b6f81d977288dba6c5686e94e0da5b10f4b51
naimiskandar22 <naimiskandar22@gmail.com>
2020-02-02 11:25:15 +0800

    Remove outdated project content

--- Assets/Scripts/Inventory/Item.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class Item : ScriptableObject
-{
-    public string ItemName;
-    public Sprite Icon;
-}
--- Assets/Scripts/Inventory/ItemLabel.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ItemLabel : MonoBehaviour
-{
-    public Item myLabel;
-}
